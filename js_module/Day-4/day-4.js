f73395f23aa6963f26525be0395a6575e554a410
Shivam Jha <110522492+geekyjha9@users.noreply.github.com>
2025-01-23 13:09:19 +0530

    operator and conditional statement have completed

---
+++ js_module/Day-4/day-4.js
@@ -0,0 +1,105 @@
+console.log("Testing ......")
+
+var a = 15;
+var b = '15';
+
+// Arithomatic Operators 
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+
+// Comparision Operators 
+
+// == :- Equal to (Note: Lightly check)
+
+console.log(a == b); // true
+
+
+console.log(a != b); // false
+
+
+
+// === :- Equal value and equal type  (Note: Strictly check)
+
+console.log(a===b);  // false
+console.log(a!==b);  // true
+
+
+
+// greater than (>)
+
+let x=10;
+let y=12;
+
+console.log(x>y); // false
+
+// less than (<)
+
+console.log(x<y); // true
+
+// greater than or equal to (>=)
+
+console.log(40>=40);
+
+// less than or equal to (<=)
+
+console.log(40<=30); // false
+console.log(40<=50); // true
+console.log(50<=50); // true
+
+
+// Logical Operators
+
+
+console.log(10>5 && 10>20); // false 
+console.log(10>5 && 10<20); // true
+
+
+console.log(10>5 || 10>20); // true
+console.log(10<5 || 10>20); // false
+
+console.log(10>5);
+console.log(!(10>5)); 
+
+
+let manishMarks = 70;
+let cutoffMarks = 32;
+
+// if Statement 
+if(manishMarks >= cutoffMarks){
+    console.log("Welcome to Semester 8.... ");
+}
+ 
+// if-else Statement 
+if(manishMarks >= cutoffMarks){
+    console.log("Welcome to Semester 8.... ");
+}
+else{
+    console.log("Sorry, Try again next year....");
+}
+
+// else-if Statement 
+
+
+if(false) {
+    console.log("Manish Aalu le aana .... ");
+}
+else if(false){
+    let X = 10;
+    console.log("Manish Soya le aana .... ");
+}
+else if(true){
+    console.log("Manish Paneer le aana .... ");
+}
+else if(true){
+    console.log("Manish Machhli le aana .... ");
+}
+else{
+    console.log("Manish Non Veg aur Milk le aana .... ");
+}
+
+var X =234809675084;
+X = 30;
+
+var X = 10;
