f498225878ad390b225770f34710a7334cdf15ee
Shivam Jha <110522492+geekyjha9@users.noreply.github.com>
2025-03-29 23:47:00 +0530

    le push kar diya

---
+++ React/Day-1/first-app/src/App.js
@@ -0,0 +1,15 @@
+import pogo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>Hello Bhaiya Ji, Ka haal ba ?</h1>
+      <h3>Green!!!!!</h3>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/Day-1/first-app/src/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+function Contact() {
+  return (
+    <div>
+      <Navbar />
+      This is Contact Page...
+    </div>
+  )
+}
+
+export default Contact
